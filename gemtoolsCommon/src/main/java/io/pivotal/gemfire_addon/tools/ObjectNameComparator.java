aed8f8e1dd50b4a43a1ce13feb9034b38bf709fd
Neil Stevenson <neil.stevenson-contractor@adp.com>
2015-05-15 10:43:03 +0100

    Comparator should implement serializable

--- gemtoolsCommon/src/main/java/io/pivotal/gemfire_addon/tools/ObjectNameComparator.java
+++ gemtoolsCommon/src/main/java/io/pivotal/gemfire_addon/tools/ObjectNameComparator.java
@@ -1,10 +1,12 @@
 package io.pivotal.gemfire_addon.tools;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
 import javax.management.ObjectName;
 
-public class ObjectNameComparator implements Comparator<ObjectName>{
+public class ObjectNameComparator implements Comparator<ObjectName>, Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public int compare(ObjectName left, ObjectName right) {
